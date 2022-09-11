7f67d89e01b3c3f6d2f3c08e1bb3ebc6de39e23f
Wellington Defassio <well.defassio@gmail.com>
2022-09-11 18:33:37 -0300

    Initial commit

---
+++ src/test/java/com/poke/project/PokeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poke.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
