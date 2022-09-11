f6a3318d8ac4c8b4b297953397d3655b15a9638d
Wellington Defassio <well.defassio@gmail.com>
2022-09-11 18:33:45 -0300

    first commit - pokemon-project

--- src/main/java/com/poke/project/PokeProjectApplication.java
+++ src/main/java/com/poke/project/PokeProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PokeProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PokeProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PokeProjectApplication.class, args);
+    }
 
 }
--- src/test/java/com/poke/project/PokeProjectApplicationTests.java
+++ src/test/java/com/poke/project/PokeProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PokeProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
